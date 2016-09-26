b0dc9eac991d883b2f621c8cd2cd27afabdc264c
Davide Sanfilippo <da-san@users.noreply.github.com>
2016-09-26 13:01:27 +0200

    add ENG label

--- index.php
+++ index.php
@@ -21,11 +21,11 @@ $url = $_POST['youtube-video'];
 
 if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[\w\-?&!#=,;]+/[\w\-?&!#=/,;]+/|(?:v|e(?:mbed)?)/|[\w\-?&!#=,;]*[?&]v=)|youtu\.be/)([\w-]{11})(?:[^\w-]|\Z)%i', $url, $match)) {
     print '<div class="yt-img">';
-    print '<p class="title-img">IMMAGINE IN HD</p>';
+    print '<p class="title-img">HD IMAGE</p>';
     print '<img class="image-hd" src="' . "http://img.youtube.com/vi/" . $match[1] . "/maxresdefault.jpg" . '" />' . "<br />";
-    print '<p class="title-img">IMMAGINE NORMAL</p>';
+    print '<p class="title-img">NORMAL IMAGE</p>';
     print '<img class="image-1"  src="' . "http://img.youtube.com/vi/" . $match[1] . "/0.jpg" . '" />' . "<br />";
-    print '<p class="title-img">IMMAGINI THUMB BASSA RISOLUZIONE</p>';
+    print '<p class="title-img">LOW DEFINITION IMAGES</p>';
     print '<div class="yt-img-thumb">';
     print '<img class="image-2"  src="' . "http://img.youtube.com/vi/" . $match[1] . "/1.jpg" . '" />';
     print '<img class="image-3"  src="' . "http://img.youtube.com/vi/" . $match[1] . "/2.jpg" . '" />';
